c6add1370735ec6f943d8a72130b6890b8fc1329
Nathaniel Horgan <nathanielhorgan@gmail.com>
2023-12-13 19:55:34 -0800

    Updated Script_Interpreter.py with more robust error handling, bug fix in easyaskForScript:
    Added Try Catch blocks when Opening files:
    Fixed control flow in easy_ask, so that we check to see if the script is valid:

--- scripts/!Lite scripts/script_interpreter.py
+++ scripts/!Lite scripts/script_interpreter.py
@@ -25,20 +25,29 @@ def startswith_fromlist(line_to_read, list) -> bool:
 
 def easy_ask_for_script() -> str:
     scriptname = input("Please input the name of the script: ")
-    print(f"Looking for script {scriptname.upper()}...")
-    os.chdir('./scripts/!Lite scripts/')
-    for filename in os.listdir('.'):
-        if os.path.isfile(filename):
-            with open(filename, encoding='utf-8') as file:
-                for index, line in enumerate(file):
-                    print(f"Looking at {file.name} at index {index+1}...")
-                    if scriptname.upper() in line.upper() and index < 6:
-                        print(f"in {file.name}, found {scriptname.upper()} in line {index+1}!")
-                        return os.path.basename(file.name)
-    if scriptname == "":
-        print("WARNING: no script given, using default!")
-        scriptname = "Protoss 1 - Zerg Town.asc3" #Temp default for easy debugging
+    if scriptname != "":
+        print(f"Looking for script {scriptname.upper()}...")
+        os.chdir('./scripts/!Lite scripts/')
+        for filename in os.listdir('.'):
+            if os.path.isfile(filename):
+                try:
+                    with open(filename, encoding='utf-8') as file:
+                        for index, line in enumerate(file):
+                            print(f"Looking at {file.name} at index {index+1}...")
+                            if scriptname.upper() in line.upper() and index < 6:
+                                print(f"in {file.name}, found {scriptname.upper()} in line {index+1}!")
+                                return os.path.basename(file.name)
+                except FileNotFoundError as e:
+                    print(f"ERROR: FILE CANNOT BE FOUND! easy_ask_for_script:  {e}") # Not needed but added regardless:
+                except IOError as e:
+                    print(f"ERROR: CANNOT OPEN FILE! IO EXCEPTION! easy_ask_for_script:  - {e}")
+    else:
+        scriptname = "Protoss 1 - Zerg Town.asc3"
+        print("WARNING: NO SCRIPT GIVEN: USING DEFAULT: " + scriptname)    
         return scriptname
+    
+    print("WARNING! CANNOT FIND SCRIPT: ERRORS MAY OCCUR: " + scriptname)
+    return scriptname
 
 def valid_type(type_to_check, variable) -> bool:
     '''Checks if a conversion is valid. If yes, return True.'''
@@ -50,33 +59,53 @@ def valid_type(type_to_check, variable) -> bool:
                 print(err)
                 return False
             return True
+        
 def read_file(filename: str) -> list[str]:
-    with open(filename, "r") as script:
-        lines = script.readlines()
+    try:
+        with open(filename, "r") as script:
+            lines = script.readlines()
         return lines
-
+    except FileNotFoundError as e:
+        print(f"ERROR: FILE CANNOT BE FOUND! read_file:  {e}")
+        return []
+    except IOError as e:
+        print(f"ERROR: CANNOT OPEN FILE! IO EXCEPTION! read_file:  - {e}")
+        return []
+    
 def delete_identified_line(filename, identifier:str):
     lines = read_file(filename)
-    with open(filename, "w") as script:
-        for line in lines:
-            if identifier not in line:
-                script.write(line) 
+    if lines != []:
+        try:
+            with open(filename, "w") as script:
+                for line in lines:
+                    if identifier not in line:
+                        script.write(line)
+        except FileNotFoundError as e:
+            print(f"ERROR: FILE CANNOT BE FOUND! delete_identified_line:  {e}")
+        except IOError as e:
+            print(f"ERROR: CANNOT OPEN FILE! IO EXCEPTION! delete_identified_line - {e}")
+    else:
+        print(f"ERROR: COULD NOT PARSE LINES ARRAY: Exiting delete_identified_line: " + filename)
 
 def find_attacks(filename):
     lines = read_file(filename)
-    attack_add_list: list = []
-    attack_list: list = []
-    for (index, line), (next_index, next_line) in pairwise(enumerate(lines)):
-        #pairwise - return result for this iteration + peek at next and return that result
-        if line.startswith("attack_add"):
-            attack_line = line.split(" ")
-            local_atkline = atk_line(index, attack_line[1], attack_line[2])
-            attack_add_list.append(local_atkline) #0 is discarded
-            if startswith_fromlist(next_line, ATK_DECLARATIONS):
-                local_attack = constructed_attack(attack_add_list, next_line)
-                attack_add_list: list = [] #reset list when one full attack is added
-                attack_list.append(local_attack)
-    return attack_list if not len(attack_list) == 0 else attack_add_list
+    if lines != []:
+        attack_add_list: list = []
+        attack_list: list = []
+        for (index, line), (next_index, next_line) in pairwise(enumerate(lines)):
+            #pairwise - return result for this iteration + peek at next and return that result
+            if line.startswith("attack_add"):
+                attack_line = line.split(" ")
+                local_atkline = atk_line(index, attack_line[1], attack_line[2])
+                attack_add_list.append(local_atkline) #0 is discarded
+                if startswith_fromlist(next_line, ATK_DECLARATIONS):
+                    local_attack = constructed_attack(attack_add_list, next_line)
+                    attack_add_list: list = [] #reset list when one full attack is added
+                    attack_list.append(local_attack)
+        return attack_list if not len(attack_list) == 0 else attack_add_list
+    else:
+        print(f"ERROR: COULD NOT PARSE LINES ARRAY: Exiting find_attacks: " + filename)
+    return []
 
 def list_attacks_from(filename):
     for attack in find_attacks(scriptname):
